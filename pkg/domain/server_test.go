3815b9f0120f483a2dc9d83c1b7b7f908f453325
Emmrys-Jay <jonathanemma121@gmail.com>
2023-01-18 02:29:56 +0100

    test: fix bug in set-liveness server tests

--- pkg/domain/server_test.go
+++ pkg/domain/server_test.go
@@ -22,24 +22,20 @@ var data = []testData{
 
 func TestSetLiveness(t *testing.T) {
 	s := domain.Server{}
-	go func() {
-		r := s.SetLiveness(true)
-		if r {
-			t.Errorf("Expected server liveness to be true")
-		}
-	}()
+	r := s.SetLiveness(true)
+	if r {
+		t.Errorf("Expected server liveness to be true")
+	}
 
 	for _, val := range data {
 		t.Run(val.name, func(t *testing.T) {
-			go func() {
-				server := domain.Server{}
-				_ = server.SetLiveness(val.firstValue)
-				result := server.SetLiveness(val.secondValue)
+			server := domain.Server{}
+			_ = server.SetLiveness(val.firstValue)
+			result := server.SetLiveness(val.secondValue)
 
-				if result != val.expected {
-					t.Errorf("Expected server liveness to be '%v'", val.expected)
-				}
-			}()
+			if result != val.expected {
+				t.Errorf("Expected server liveness to be '%v'", val.expected)
+			}
 		})
 	}
 
